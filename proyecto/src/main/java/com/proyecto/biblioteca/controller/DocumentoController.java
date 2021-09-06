b0f93875b6abb6503d9ba32f497dad719287955c
Josselyn Flores Paredes <josyuk_3000@hotmail.com>
2021-09-06 10:29:23 -0500

    rename controller

--- proyecto/src/main/java/com/proyecto/biblioteca/controller/DocumentoController.java
+++ proyecto/src/main/java/com/proyecto/biblioteca/controller/DocumentoController.java
@@ -1,5 +1,7 @@
 package com.proyecto.biblioteca.controller;
 
+import com.proyecto.biblioteca.model.Documento;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -8,10 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import java.util.Optional;
-
-import com.proyecto.biblioteca.model.Documento;
-import com.proyecto.biblioteca.repository.BibliotecaRepository;
 
 @Controller
 @RequestMapping("/documento")
